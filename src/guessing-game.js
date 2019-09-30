644e4064409324b9bff650173c4a5976e995e2a2
nextdesusu <nextdesusu@gmail.com>
2019-09-30 15:05:31 +0400

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.biggest = 0;
+        this.smallest = 0;
+        this.answer = 0;
+    }
 
     setRange(min, max) {
-
+        this.smallest = min;
+        this.biggest = max;
     }
 
     guess() {
-
+        this.answer = this.smallest + Math.round((this.biggest - this.smallest) / 2);
+        return this.answer;
     }
 
     lower() {
-
+        this.biggest = this.answer;
     }
 
     greater() {
-
+        this.smallest = this.answer;
     }
 }
 
